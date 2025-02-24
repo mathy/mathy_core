43e216f80b1d7c1a4c8b9183c8cacce864d9d4dc
Justin DuJardin <justin@dujardinconsulting.com>
2025-02-23 18:33:29 -0800

    chore: fix remove type ignore

--- mathy_core/util.py
+++ mathy_core/util.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass, field
 from typing import Any, Dict, List, NamedTuple, Optional, Set, Tuple, Union, cast
 
 import numpy as np
-from wasabi import TracebackPrinter
+from wasabi import TracebackPrinter  # type: ignore
 
 from .expressions import (
     AddExpression,
